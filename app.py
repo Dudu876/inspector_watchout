b6ae2197a0506bff00137d23e9652f639596a046
dudu-dy <dudu.lemberberg@dynamicyield.com>
2019-04-05 01:09:56 +0300

    initial application

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from bson.json_util import dumps
+from datetime import datetime
+import pymongo
+import json
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb://admin:Aa123456@ds231956.mlab.com:31956/inspector")
+db = client.inspector
+
+
+@app.route('/api/get_all', methods=['GET'])
+def get_all():
+    cursor = db.reports.find({})
+    res = []
+    for doc in cursor:
+        res.append(dumps(doc))
+
+    return jsonify(res)
+
+
+@app.route('/api/insert', methods=['POST'])
+def insert():
+    body = request.get_json()
+    body['timestamp'] = datetime.now()
+    db.reports.insert_one(body)
+    return jsonify({"status": "ok"})
+
+
+if __name__ == "__main__":
+    app.run()
